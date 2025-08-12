f4e359c9f785d62ea640bc5158a4aa63de9025bd
gayeongge <gydiane1008@gmail.com>
2025-08-12 17:36:30 +0900

    gemma3 tool add

--- main/chatbot/node/react.py
+++ main/chatbot/node/react.py
@@ -1,17 +1,22 @@
 import os
 
-from langchain.agents import create_react_agent, AgentExecutor
+from langchain.agents import AgentExecutor, initialize_agent, AgentType
 from langchain.prompts import PromptTemplate
+from langchain_core.tools import Tool
 from langchain_ollama import ChatOllama
+
+from win32comext.adsi.demos.scp import verbose
 
 from state.schema import OutputState
 
-
+from dotenv import load_dotenv
+load_dotenv()
 
 # ReAct용 필수 변수 포함 프롬프트
 REACT_PROMPT = PromptTemplate(
     input_variables=["input", "agent_scratchpad", "tools", "tool_names"],
     template=(
+        "You must always use the following format and never reply in any other way."
         "You are a helpful assistant. You have access to the following tools:\n"
         "{tools}\n"
         "Use the following format:\n"
@@ -35,9 +40,29 @@ class ReactAgentNode:
     def __init__(self):
         self.base_url = os.environ.get("OLLAMA_BASE_URL", "")
         llm = ChatOllama(model="gemma3:1b", streaming=True, base_url=self.base_url)
-        tools = []  # 사용할 툴이 있다면 여기에 추가
-        agent = create_react_agent(llm, tools, prompt=REACT_PROMPT)
-        self.executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+        add_tool = Tool(
+            name="Calculator",
+            func=self.add_numbers,
+            description="Adds numbers given as space-separated input. Example input: '3 5'"
+        )
+        tools = [add_tool]  # 사용할 툴이 있다면 여기에 추가
+        # agent = create_react_agent(llm, tools, state_modifier=REACT_PROMPT)
+        agent = initialize_agent(
+            tools,
+            llm,
+            agent= AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+            verbose=True
+        )
+        # self.executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+        self.executor = agent
+
+    @staticmethod
+    def add_numbers(input:str)-> str:
+        try:
+            return str("10")
+        except Exception:
+            return "Error..."
+
 
     def __call__(self, state)-> OutputState:
         # state는 TypedDict로 input 키를 가짐
